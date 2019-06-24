e1371ab5585d8c4a7f01dbd1ea47cae0f5bfc3e5
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-06-25 03:34:01 +0800

    Added jest in favour of cypress

--- test/e2e/integration/sample.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-describe('Sample tests', () => {
-  it('Visits index page', () => {
-    cy.visit('/');
-    cy.contains('div', 'Welcome to the Vuetify + Nuxt.js template');
-    cy.contains('p', 'Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.');
-  });
-});
