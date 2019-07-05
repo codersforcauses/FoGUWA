11b8e6d199cdaebc5138dadbdf14f800fe839a97
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-07-06 04:47:56 +0800

    made nav drawer a modular component

---
+++ components/other/NavDrawer.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+    temporary
+    floating
+    app
+  >
+    <v-list>
+      <v-list-tile
+        v-for="(item, i) in items"
+        :key="i"
+        :to="item.to"
+        router
+        exact
+      >
+        <v-list-tile-action>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-tile-action>
+        <v-list-tile-content>
+          <v-list-tile-title v-text="item.title" />
+        </v-list-tile-content>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: {
+    drawer: Boolean
+  },
+  data() {
+    return {
+      items: [
+        {
+          icon: 'apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'bubble_chart',
+          title: 'Inspire',
+          to: '/inspire'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
