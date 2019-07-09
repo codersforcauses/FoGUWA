a8eb03a16d1b28f5390a16408ef4a40c9a99fe2d
jtmalarecki <jt.malarecki@gmail.com>
2019-07-09 15:40:16 +0800

    Made leaf slightly prettier.

--- components/GoogleMapMarker.vue
+++ components/GoogleMapMarker.vue
@@ -24,7 +24,7 @@ export default {
       path: 'M17 8C8 10 5.901 16.166 3.816 21.343l1.891.65.954-2.292c.482.168.976.299 1.339.299C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z',
       fillColor: '#008000',
       fillOpacity: 1.0,
-      strokeColor: '#000000',
+      strokeColor: '#004d00',
       scale: 1
     }
     this.markerInst = new this.google.maps.Marker({
