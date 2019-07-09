b4a9cb84e615c87d60593473c72f16c9cd2a1e0d
jtmalarecki <jt.malarecki@gmail.com>
2019-07-09 15:04:17 +0800

    Made CSS scoped.

--- components/GoogleMapLoader.vue
+++ components/GoogleMapLoader.vue
@@ -72,7 +72,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 /* Always set the map height explicitly to define the size of the div
  * element that contains the map. */
 .google-map {
