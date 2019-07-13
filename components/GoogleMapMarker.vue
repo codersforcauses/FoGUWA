5e589f7c67523d948394965935d91abf67f12705
jtmalarecki <jt.malarecki@gmail.com>
2019-07-13 15:17:32 +0800

    Added SVG to map marker template.

--- components/GoogleMapMarker.vue
+++ components/GoogleMapMarker.vue
@@ -1,3 +1,25 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    focusable="false"
+    width="1em"
+    height="1em"
+    style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M17 8C8 10 5.901 16.166 3.816 21.343l1.891.65.954-2.292c.482.168.976.299 1.339.299C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z"
+      fill="green"
+      fillColor="#008000"
+      fillOpacity="1.0"
+      strokeColor="#004d00"
+    />
+  </svg>
+</template>
+
 <script>
 export default {
   props: {
