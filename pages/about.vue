8e6153207220dc424723bdf7f03b28579a91642a
22358769 <22358769@student.uwa.edu.au>
2019-07-13 11:00:13 +0800

    Update about.vue
    Added proper list

--- pages/about.vue
+++ pages/about.vue
@@ -21,7 +21,13 @@
           About Friends of the Grounds of UWA
         </v-card-title>
         <v-card-text style="text-align: justify">
-          <p>Friends of the Grounds UWA (FoGUWA) was established in the 2000 with four core objectives: (1) to encourage the University and wider Western Australian community to take an active interest in UWA’s grounds as an important part of the heritage of this State; (2) to enhance the profile of the university’s grounds by developing an annual program of activities; (3) to provide information about the grounds through publications, lectures and events; (4) to raise funds to assist with special projects relating to the grounds.</p>
+          <p>Friends of the Grounds UWA (FoGUWA) was established in the 2000 with four core objectives: </p>
+          <ol style="margin-bottom: 16px;">
+            <li>To encourage the University and wider Western Australian community to take an active interest in UWA’s grounds as an important part of the heritage of this State</li>
+            <li>To enhance the profile of the university’s grounds by developing an annual program of activities</li>
+            <li>To provide information about the grounds through publications, lectures and events</li>
+            <li>To raise funds to assist with special projects relating to the grounds</li>
+          </ol>
           <a target="_blank" href="https://www.facebook.com/friendsofthegrounds/">FoGUWA Facebook page</a>
         </v-card-text>
       </v-flex>
