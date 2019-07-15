85bb1d5dd49cf14ca4a157810e61f24748345933
jtmalarecki <jt.malarecki@gmail.com>
2019-07-15 11:53:28 +0800

    Reworked marker to rely on an SVG component.

--- components/GoogleMapMarker.vue
+++ components/GoogleMapMarker.vue
@@ -1,5 +1,6 @@
 <template>
   <svg
+    ref="leafIconSVG"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     aria-hidden="true"
@@ -12,10 +13,9 @@
   >
     <path
       d="M17 8C8 10 5.901 16.166 3.816 21.343l1.891.65.954-2.292c.482.168.976.299 1.339.299C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z"
-      fill="green"
-      fillColor="#008000"
+      :fill="fillColour"
       fillOpacity="1.0"
-      strokeColor="#004d00"
+      :stroke="strokeColour"
     />
   </svg>
 </template>
@@ -34,6 +34,26 @@ export default {
     marker: {
       type: Object,
       required: true
+    },
+    iconName: {
+      type: String,
+      default: 'leaf'
+    },
+    width: {
+      type: Number,
+      default: 24
+    },
+    height: {
+      type: Number,
+      default: 24
+    },
+    fillColour: {
+      type: String,
+      default: '#008000'
+    },
+    strokeColour: {
+      type: String,
+      default: '#004d00'
     }
   },
   data() {
@@ -42,17 +62,17 @@ export default {
     }
   },
   mounted() {
-    const leaf = {
-      path: 'M17 8C8 10 5.901 16.166 3.816 21.343l1.891.65.954-2.292c.482.168.976.299 1.339.299C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z',
-      fillColor: '#008000',
-      fillOpacity: 1.0,
-      strokeColor: '#004d00',
-      scale: 1
+    const leafIcon = {
+      url: 'data:image/svg+xml;utf8,' + encodeURIComponent(this.$refs.leafIconSVG.outerHTML),
+      size: null, // new this.google.maps.Size(24, 24),
+      origin: null, // new this.google.maps.Point(0, 0),
+      anchor: null, // new this.google.maps.Point(12, 12),
+      scaledSize: new this.google.maps.Size(this.width, this.height)
     }
     this.markerInst = new this.google.maps.Marker({
       label: this.marker.label,
       position: this.marker.position,
-      icon: leaf,
+      icon: leafIcon,
       map: this.map
     })
   }
