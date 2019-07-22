f0bad182c41250c00ad8f3fd6030d5a7dcbf64bb
jtmalarecki <jt.malarecki@gmail.com>
2019-07-22 11:25:22 +0800

    Prototyped installation button.

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,14 @@
     justify-center
     align-center
   >
+    <button
+      v-show="canInstall"
+      ref="install"
+      absolute
+      @click="installApp"
+    >
+      Add to home screen
+    </button>
     <v-card
       tile
       height="100%"
@@ -18,11 +26,54 @@
 <script>
 import UWAPlantMap from '~/components/UWAPlantMap.vue'
 import Searchbar from '~/components/other/Searchbar.vue'
+let deferredPrompt
 
 export default {
   components: {
     UWAPlantMap,
     Searchbar
+  },
+  data() {
+    return {
+      canInstall: false
+    }
+  },
+  created() {
+    console.log('test') // eslint-disable-line
+    if (process.browser) {
+      window.addEventListener('beforeinstallprompt', this.installPrompt) // eslint-disable-line
+    }
+  },
+  destroyed() {
+    if (process.browser) {
+      window.removeEventListener('beforeinstallprompt', this.installPrompt) // eslint-disable-line
+    }
+  },
+  methods: {
+    installPrompt(e) {
+      console.log('install prompt') // eslint-disable-line
+      // Prevent Chrome 67 and earlier from automatically showing the prompt
+      e.preventDefault()
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e
+      // Show install button
+      this.canInstall = true
+    },
+    installApp() {
+      // Hide the button
+      this.canInstall = false
+      // Show the deffered install prompt
+      deferredPrompt.prompt()
+      // Wait for the user to respond to the prompt
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt') // eslint-disable-line
+        } else {
+          console.log('User dismissed the A2HS prompt') // eslint-disable-line
+        }
+        deferredPrompt = null
+      })
+    }
   }
 }
 </script>
