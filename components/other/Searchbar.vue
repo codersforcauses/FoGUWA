d38dc0d3bd96782f0cb69ec1e27f57bc22b4a8bc
jtmalarecki <jt.malarecki@gmail.com>
2019-07-23 16:30:53 +0800

    Implemented search for plants stored in json db.

--- components/other/Searchbar.vue
+++ components/other/Searchbar.vue
@@ -4,9 +4,19 @@
     absolute
     style="margin: 10px;"
   >
-    <v-text-field
-      hide-details
+    <v-autocomplete
+      v-model="searchModel"
+      :filter="searchFilter"
+      :items="searchItems"
+      :loading="searchLoading"
+      :search-input.sync="searchQuery"
+      clearable
+      hide-no-data
+      hide-selected
+      item-text="displayName"
+      placeholder="Search..."
       prepend-icon="search"
+      return-object
       single-line
     />
 
@@ -21,9 +31,83 @@
 </template>
 
 <script>
+import { count, plants } from '@/assets/plantdb.json'
+
+export default {
+  props: {
+    searchResults: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data: function () {
+    return {
+      plants: [],
+      searchDescLimit: 60,
+      searchLoading: false,
+      searchModel: null,
+      searchQuery: null
+    }
+  },
+  computed: {
+    searchItems() {
+      if (!this.plants) {
+        return []
+      }
+      return this.plants.map((entry) => {
+        const description = entry.name + ' (' + entry.scientificName + ')'
+        const name = description.length > this.searchDescLimit
+          ? description.slice(0, this.searchDescLimit - 3) + '...'
+          : description
+        return Object.assign({}, entry, { 'displayName': name })
+      })
+    }
+  },
+  watch: {
+    searchModel(val) {
+      const results = []
+      if (val != null) {
+        results.push(val)
+      }
+      this.$emit('update:search-results', results)
+    },
+    searchQuery(val) {
+      // Items have already been loaded
+      if (this.searchItems.length > 0) return
+      // Items have already been requested
+      if (this.searchLoading) return
+      this.searchLoading = true
+      // Load the input items
+      this.plants = plants
+      this.counts = count
+      this.searchLoading = false
+      // TODO: query database for matches, e.g.
+      // queryDatabasePromise('val').then((res) => {
+      //   return res.json()
+      // }).then((res) => {
+      //   const { count, plants } = res
+      //   this.count = count
+      //   this.plants = plants
+      // }).catch((err) => {
+      //   // TODO: emit log error message
+      // })
+      // .finally(() => {
+      //   this.searchLoading = false
+      // })
+    }
+  },
+  methods: {
+    searchFilter(plant, queryText, itemText) {
+      const caseOne = plant.name.toLowerCase()
+      const caseTwo = plant.scientificName.toLowerCase()
+      const searchText = queryText.toLowerCase()
+
+      return caseOne.indexOf(searchText) > -1 ||
+        caseTwo.indexOf(searchText) > -1
+    }
+  }
+}
 
 </script>
-
-<style>
-
-</style>
