65228ec116c1c330a90553d776754ec8c2d7a0e1
JeremiahPinto <jeremiahpinto279@outlook.com>
2019-08-03 20:31:17 +0800

    Removed searchbar

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,15 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-card
-      tile
-      height="100%"
-      width="100%"
-    >
-      <Searchbar />
-      <UWAPlantMap />
-    </v-card>
+  <v-layout>
+    <UWAPlantMap />
   </v-layout>
 </template>
 
 <script>
 import UWAPlantMap from '~/components/UWAPlantMap.vue'
-import Searchbar from '~/components/other/Searchbar.vue'
 
 export default {
   components: {
-    UWAPlantMap,
-    Searchbar
+    UWAPlantMap
   }
 }
 </script>
