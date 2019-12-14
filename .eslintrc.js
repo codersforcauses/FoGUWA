a351a4ebe10ee3e1859aac6c022cbbebed2029b6
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-12-14 13:45:00 +0800

    eslint update with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
+    'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended'
@@ -17,6 +18,16 @@ module.exports = {
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
-    'vue/html-quotes': ['error', 'double']
+    'vue/html-quotes': ['error', 'double'],
+    'vue/max-attributes-per-line': [
+      2,
+      {
+        singleline: 20,
+        multiline: {
+          max: 1,
+          allowFirstLine: false
+        }
+      }
+    ]
   }
 }
