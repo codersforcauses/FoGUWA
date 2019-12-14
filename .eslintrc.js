93e57282fed7eb2eb5f938c5abf9b09e2dfd6917
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-12-14 14:13:29 +0800

    remove dep

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended'
-    // 'plugin:nuxt/recommended'
   ],
   plugins: ['prettier'],
   // add your custom rules here
