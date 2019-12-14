f115fcf4cedcd0147135c015968154297ea165b1
Jeremiah Pinto <jeremiahpinto279@outlook.com>
2019-12-14 14:04:45 +0800

    remove nuxt plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
     '@nuxtjs',
     'prettier',
     'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:prettier/recommended'
+    // 'plugin:nuxt/recommended'
   ],
   plugins: ['prettier'],
   // add your custom rules here
