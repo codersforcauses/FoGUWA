1571596e59426f938f93c6ac27ff1a013491ad6d
Drewbi <22423862@student.uwa.edu.au>
2019-12-20 19:19:23 +0800

    Added user and flora seeding

---
+++ server/seeder/index.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const consola = require('consola')
+const User = mongoose.model('User')
+const Flora = mongoose.model('Flora')
+const { userSeedData, floraSeedData } = require('./seedData')
+
+const addUser = userObject => {
+  return new Promise((resolve, reject) => {
+    const user = new User()
+    user.name.first = userObject.name.first
+    user.name.last = userObject.name.last
+    user.email = userObject.email
+    user.password = userObject.password
+    user.save(err => {
+      if (err) {
+        reject(err)
+      }
+    })
+    resolve(user)
+  })
+}
+
+const addFlora = floraObject => {
+  return new Promise((resolve, reject) => {
+    const flora = new Flora()
+    flora.name = floraObject.name
+    flora.scientificName = floraObject.scientificName
+    flora.description = floraObject.description
+    flora.icon = floraObject.icon
+    flora.instances = floraObject.instances
+    flora.save(err => {
+      if (err) {
+        reject(err)
+      }
+    })
+    resolve(flora)
+  })
+}
+
+// Seed the db
+module.exports.seedUsers = async () => {
+  const userPromises = userSeedData.map(userSeed => addUser(userSeed))
+  const users = await Promise.all(userPromises)
+  consola.success(`Seeded ${users.length} users`)
+}
+
+module.exports.seedFlora = async () => {
+  const floraPromises = floraSeedData.map(floraSeed => addFlora(floraSeed))
+  const flora = await Promise.all(floraPromises)
+  consola.success(`Seeded ${flora.length} plants`)
+}
