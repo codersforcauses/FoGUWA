f9c0d5527ac9cea97723a559f0b1ed8fc885a265
Dean Vains <dean.vains@outlook.com>
2019-12-22 16:55:31 +0800

    fix(flora.js): tested all routes in Flora, Issues with Put and Post
    Tested all routes in Flora, Issues with Put and Post.

--- server/routes/flora.js
+++ server/routes/flora.js
@@ -6,16 +6,14 @@ const router = express.Router()
 
 router.get('/flora', async (req, res, next) => {
   const floraObj = await Flora.find()
-
   res.json(floraObj)
   next()
 })
 
 router.post('/flora', async (req, res, next) => {
   const { addFlora } = require('../seeder/index')
-  const floraPromise = await addFlora(req.body)
-  const flora = await Promise.all(floraPromise)
-  res.json(flora)
+  const floraPromise = await addFlora(req.params)
+  await res.json(floraPromise)
   next()
 })
 
@@ -26,23 +24,27 @@ router.get('/flora/:id', async (req, res, next) => {
 })
 
 router.put('/flora/:id', async (req, res, next) => {
-  const floraObj = await Flora.findById(req.params.id)
-
-  floraObj.update(
-    { name: res.body.name },
-    { scientificName: res.body.scientificName },
-    { description: res.body.description },
-    { icon: res.body.icon },
-    { instances: res.body.instances }
-  )
-
+  const filter = { id: req.params.id }
+  const update = {
+    name: req.body.name,
+    scientificName: req.body.scientificName,
+    description: req.body.description,
+    icon: req.body.icon,
+    instances: req.body.instances
+  }
+  const floraObj = await Flora.findOneAndUpdate(filter, update, (err, doc) => {
+    if (err) {
+      console.log('Something wrong when updating data!')
+    }
+  })
   res.json(floraObj)
   next()
 })
 
 router.delete('/flora/:id', async (req, res, next) => {
   const floraObj = await Flora.findByIdAndDelete(req.params.id)
-  res.json(floraObj.deletedCount)
+  const flora = await Promise.all(floraObj)
+  res.json(flora)
   next()
 })
 
