7c7521828c1d915a1d4de8398592cc22a30adc0e
Drewbi <22423862@student.uwa.edu.au>
2019-12-23 16:05:44 +0800

    Removed changes from .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,10 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true,
-    es6: true
+    node: true
   },
   parserOptions: {
-    parser: 'babel-eslint',
-    ecmaVersion: 2018
+    parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
@@ -16,7 +14,7 @@ module.exports = {
     'plugin:nuxt/base',
     'plugin:prettier/recommended'
   ],
-  plugins: ['prettier', 'eslint'],
+  plugins: ['prettier'],
   // add your custom rules here
   rules: {
     'nuxt/no-cjs-in-config': 'off',
