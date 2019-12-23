f4cf6ebabcc7e080ca71650346dcb6b36a06703b
Drewbi <22423862@student.uwa.edu.au>
2019-12-23 16:12:04 +0800

    Removed eslintrc rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,13 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    es6: true
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
+    ecmaVersion: 2017
+
   },
   extends: [
     '@nuxtjs',
@@ -14,20 +17,5 @@ module.exports = {
     'plugin:nuxt/base',
     'plugin:prettier/recommended'
   ],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {
-    'nuxt/no-cjs-in-config': 'off',
-    'vue/html-quotes': ['error', 'double'],
-    'vue/max-attributes-per-line': [
-      2,
-      {
-        singleline: 20,
-        multiline: {
-          max: 1,
-          allowFirstLine: false
-        }
-      }
-    ]
-  }
+  plugins: ['prettier']
 }
