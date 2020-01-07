5fc5e762c2ab1d4f42ff95b8f0bb81083f5fe0b3
VicJohn447 <21485762@student.uwa.edu.au>
2020-01-07 11:37:04 +0800

    improvement(adding three new plant icons treeicon.vue, tulipicon.vue and lotusicon.vue): new icons
    new icons with different colours (if you don't like the colours I can change them).

---
+++ components/icons/LotusIcon.vue
@@ -0,0 +1,51 @@
+<template>
+  <svg
+    ref="treeIconSVG"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    focusable="false"
+    width="1em"
+    height="1em"
+    style="-ms-transform: rotate(360deg); -webkit-transform: rotate(360deg); transform: rotate(360deg);"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      :fill="fillColour"
+      :stroke="strokeColour"
+      d="M15.5,9.63C15.31,6.84 14.18,4.12 12.06,2C9.92,4.14 8.74,6.86 8.5,
+      9.63C9.79,10.31 10.97,11.19 12,12.26C13.03,11.2 14.21,10.32 15.5,9.63M12,
+      15.45C9.85,12.17 6.18,10 2,10C2,20 11.32,21.89 12,22C12.68,
+      21.88 22,20 22,10C17.82,10 14.15,12.17 12,15.45Z"
+      fill-opacity="1.0"
+    />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    iconName: {
+      type: String,
+      default: 'tree'
+    },
+    width: {
+      type: Number,
+      default: 24
+    },
+    height: {
+      type: Number,
+      default: 24
+    },
+    fillColour: {
+      type: String,
+      default: '#d282a6'
+    },
+    strokeColour: {
+      type: String,
+      default: '#e8b4bc'
+    }
+  }
+}
+</script>
