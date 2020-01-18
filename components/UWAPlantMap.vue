990debc6998e0c30b63153529c39975fc6ebf0eb
Drewbi <22423862@student.uwa.edu.au>
2020-01-18 19:19:34 +0900

    Refactored to remove array destructure of plants

--- components/UWAPlantMap.vue
+++ components/UWAPlantMap.vue
@@ -71,14 +71,10 @@ export default {
         ...uwaMapSettings
       }
     },
-    markers() {
-      // return the array
-      return [...plants]
-    },
     defaultInfo() {
       return {
-        plantName: 'Plants and Trees',
-        sciName: 'Planticus Namium',
+        plantName: 'Plant Name',
+        sciName: 'Scientific Plant Name',
         images: [
           'http://www.ahachemistry.com/uploads/1/1/8/3/118378549/dsc-5454_orig.jpg',
           'http://www.ahachemistry.com/uploads/1/1/8/3/118378549/dsc-7528_orig.jpg',
@@ -122,7 +118,6 @@ export default {
     }
   },
   methods: {
-    renderPopup(marker) {},
     loadMarkers() {
       if (this.map && this.google) {
         // Clear old markers
@@ -131,7 +126,7 @@ export default {
         }
         this.markerInstances = []
         // Create new markers and store them
-        this.markers.forEach((marker, index) => {
+        plants.forEach((plant, index) => {
           const leafIcon = {
             path:
               'M17 8C8 10 5.901 16.166 3.816 21.343l1.891.65.954-2.292c.482.168.976.299 1.339.299C19 20 22 3 22 3c-1 2-8 2.25-13 3.25S2 11.5 2 13.5s1.75 3.75 1.75 3.75C7 8 17 8 17 8z',
@@ -149,20 +144,20 @@ export default {
             scale: 1
             }
           // Plot all instances
-          for (const instance of marker.instances) {
+          for (const instance of plant.instances) {
             const markerInst = new this.google.maps.Marker({
-              label: marker.name,
+              label: plant.name,
               position: instance.location,
-              icon: marker.type === 'tree' ? leafIcon : statueIcon,
+              icon: plant.type === 'tree' ? leafIcon : statueIcon,
               map: this.map
             })
             markerInst.addListener('click', () => {
               this.plantInfo = {
-                plantName: marker.name || this.defaultInfo.plantName,
-                sciName: marker.scientificName || this.defaultInfo.scientificName,
-                description: marker.description || this.defaultInfo.description,
-                images: marker.images || this.defaultInfo.images,
-                type: marker.type || 'tree'
+                plantName: plant.name || this.defaultInfo.plantName,
+                sciName: plant.scientificName || this.defaultInfo.scientificName,
+                description: plant.description || this.defaultInfo.description,
+                images: plant.images || this.defaultInfo.images,
+                type: plant.type || 'tree'
               }
               this.plantInfoVisible = true
             })
