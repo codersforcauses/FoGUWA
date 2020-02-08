518e759ce2af3e3b8f8c81c2b696357e1fffa4bd
Drewbi <22423862@student.uwa.edu.au>
2020-02-08 12:25:36 +0800

    Changed subtitle for user card

--- components/other/NavDrawer.vue
+++ components/other/NavDrawer.vue
@@ -31,7 +31,7 @@
           <v-list-item-title>
             {{ user.nickname }}
           </v-list-item-title>
-          <v-list-item-subtitle>Authenticated</v-list-item-subtitle>
+          <v-list-item-subtitle>User</v-list-item-subtitle>
         </v-list-item-content>
 
         <v-list-item-action>
