514347e2c33eaf6a3b947f3efc0df852426e47b8
Drewbi <22423862@student.uwa.edu.au>
2020-02-11 09:52:00 +0800

    Updated icon path

--- assets/js/plantIcons.js
+++ assets/js/plantIcons.js
@@ -44,7 +44,7 @@ const lotus = {
 
 const info = {
   path:
-  'M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z',
+  "M13,9H11V7h2m0,10H11V11h2M12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Z",
   fillColor: '#88BBEE'
 }
 export default { info, leaf, tree, tulip, lotus, iconStyle }
