d2009ce1989abf35e4bab77f8b9dfbba1a9e40dc
Drewbi <22423862@student.uwa.edu.au>
2020-03-07 13:29:11 +0800

    Added popup close button

--- components/other/plantCard.vue
+++ components/other/plantCard.vue
@@ -1,172 +1,185 @@
 <template>
-  <v-card class="mx-4">
-    <v-window v-model="displayForm">
-      <v-window-item :value="1">
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="150px"
-        ></v-img>
-        <v-row>
-          <v-icon large text icon color="pink lighten-3" class="ml-3 pl-6">
-            mdi-mushroom
-          </v-icon>
-          <v-col>
-            <v-card-title class="pt-2 pl-6">
-              {{ plant.name }}
-              <v-chip color="indigo darken-3" small outlined class="ml-4">
-                <i>{{ plant.scientificName }}</i>
-              </v-chip>
-            </v-card-title>
-            <!-- <v-card-subtitle class="pl-6">
-              {{ plant.instance[0].heading }}
-            </v-card-subtitle> -->
-          </v-col>
-          <v-card-actions class="mr-2">
-            <v-btn color="indigo" text dark @click="displayForm++">
-              EDIT
-            </v-btn>
-            <v-dialog v-model="confirmDelete" persistent max-width="290">
-              <template v-slot:activator="{ on }">
-                <v-btn color="indigo" text class="mr-3" v-on="on">
-                  DELETE
+  <v-window v-model="displayForm">
+    <v-window-item :value="1">
+      <v-btn
+        fab
+        absolute
+        left
+        small
+        depressed
+        color="#00000077"
+        :ripple="false"
+        class="mt-2"
+        @click="$router.go(-1)"
+      >
+        <v-icon color="white">
+          mdi-arrow-left
+        </v-icon>
+      </v-btn>
+      <v-img
+        src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+        height="150px"
+      ></v-img>
+      <v-row>
+        <v-icon large text icon color="pink lighten-3" class="ml-3 pl-6">
+          mdi-mushroom
+        </v-icon>
+        <v-col>
+          <v-card-title class="pt-2 pl-6">
+            {{ plant.name }}
+            <v-chip color="indigo darken-3" small outlined class="ml-4">
+              <i>{{ plant.scientificName }}</i>
+            </v-chip>
+          </v-card-title>
+          <!-- <v-card-subtitle class="pl-6">
+            {{ plant.instance[0].heading }}
+          </v-card-subtitle> -->
+        </v-col>
+        <v-card-actions class="mr-2">
+          <v-btn color="indigo" text dark @click="displayForm++">
+            EDIT
+          </v-btn>
+          <v-dialog v-model="confirmDelete" persistent max-width="290">
+            <template v-slot:activator="{ on }">
+              <v-btn color="indigo" text class="mr-3" v-on="on">
+                DELETE
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="headline">
+                Delete this plant
+              </v-card-title>
+              <v-card-text>probably display the name of the plant they are about to delete here</v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="indigo" text @click="confirmDelete = false">
+                  NO
                 </v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="headline">
-                  Delete this plant
-                </v-card-title>
-                <v-card-text>probably display the name of the plant they are about to delete here</v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="indigo" text @click="confirmDelete = false">
-                    NO
-                  </v-btn>
-                  <v-btn color="indigo" dark @click="confirmDelete = true">
-                    YES
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-card-actions>
-        </v-row>
-        <v-row>
-          <v-card-text class="ml-3 pt-0 pb-6 px-6">
-            {{ plant.description }}
-          </v-card-text>
-        </v-row>
-        <v-row>
-          <v-divider class="mx-3"></v-divider>
-        </v-row>
+                <v-btn color="indigo" dark @click="confirmDelete = true">
+                  YES
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-card-actions>
+      </v-row>
+      <v-row>
+        <v-card-text class="ml-3 pt-0 pb-6 px-6">
+          {{ plant.description }}
+        </v-card-text>
+      </v-row>
+      <v-row>
+        <v-divider class="mx-3"></v-divider>
+      </v-row>
 
-        <v-list>
-          <v-list-item
-            v-for="(instance,i) in plant.instance"
-            :key="i"
-            class="list-item"
-            @mouseover="showByIndex = i"
-            @mouseout="showByIndex = null"
-          >
-            <v-list-item-content>
-              <v-list-item-title>{{ instance.heading }}</v-list-item-title>
-            </v-list-item-content>
+      <v-list>
+        <v-list-item
+          v-for="(instance,i) in plant.instance"
+          :key="i"
+          class="list-item"
+          @mouseover="showByIndex = i"
+          @mouseout="showByIndex = null"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ instance.heading }}</v-list-item-title>
+          </v-list-item-content>
 
-            <v-list-item-action v-show="showByIndex === i" class="action">
-              <v-btn icon color="indigo">
-                <v-icon>mdi-pencil</v-icon>
-              </v-btn>
-              <v-btn icon color="indigo">
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-              <v-btn icon color="indigo">
-                <v-icon>my_location</v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-list>
-      </v-window-item>
-      <v-window-item :value="2">
-        <v-row class="mx-2 mb-0">
-          <v-col class="pb-0">
-            <v-text-field
-              clearable
-              clear-icon="cancel"
-              label="Name"
-              outlined
-              dense
-              rows="1"
-              :value="plant.name"
-            ></v-text-field>
-          </v-col>
-          <v-col class="pb-0">
-            <v-text-field
-              clearable
-              clear-icon="cancel"
-              label="Scientific Name"
-              no-resize
-              outlined
-              dense
-              rows="1"
-              :value="plant.scientificName"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-        <v-row>
-          <v-card-actions class="mx-7 mb-6">
-            <p class="font-weight-light ma-0">
-              Choose icon &emsp;
-            </p>
-            <v-btn-toggle v-model="icon">
-              <v-btn color="pink" icon outlined class="round mx-1">
-                <v-icon :color="icon === 0 ? 'pink' : 'gray'">
-                  mdi-flower
-                </v-icon>
-              </v-btn>
-              <v-btn color="pink lighten-3" icon outlined class="round mx-1">
-                <v-icon :color="icon === 1 ? 'pink lighten-3' : 'gray'">
-                  mdi-mushroom
-                </v-icon>
-              </v-btn>
-              <v-btn color="orange" icon outlined class="round mx-1">
-                <v-icon :color="icon === 2 ? 'orange' : 'gray'">
-                  mdi-carrot
-                </v-icon>
-              </v-btn>
-              <v-btn color="light-green" icon outlined class="round mx-1">
-                <v-icon :color="icon === 3 ? 'light-green' : 'gray'">
-                  mdi-leaf
-                </v-icon>
-              </v-btn>
-              <v-btn color="green" icon outlined class="round mx-1">
-                <v-icon :color="icon === 4 ? 'green' : 'gray'">
-                  mdi-tree
-                </v-icon>
-              </v-btn>
-            </v-btn-toggle>
-          </v-card-actions>
-        </v-row>
-        <v-row>
-          <v-textarea
+          <v-list-item-action v-show="showByIndex === i" class="action">
+            <v-btn icon color="indigo">
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+            <v-btn icon color="indigo">
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+            <v-btn icon color="indigo">
+              <v-icon>my_location</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+    </v-window-item>
+    <v-window-item :value="2">
+      <v-row class="mx-2 mb-0">
+        <v-col class="pb-0">
+          <v-text-field
             clearable
             clear-icon="cancel"
-            label="Description"
+            label="Name"
             outlined
-            :value="plant.description"
-            class="mx-8"
-            rows="10"
-          ></v-textarea>
-        </v-row>
-        <v-card-actions class="px-4 pb-4">
-          <v-btn color="indigo" text @click="displayForm--">
-            BACK
-          </v-btn>
-          <v-spacer></v-spacer>
-          <v-btn color="indigo" dark>
-            SAVE
-          </v-btn>
+            dense
+            rows="1"
+            :value="plant.name"
+          ></v-text-field>
+        </v-col>
+        <v-col class="pb-0">
+          <v-text-field
+            clearable
+            clear-icon="cancel"
+            label="Scientific Name"
+            no-resize
+            outlined
+            dense
+            rows="1"
+            :value="plant.scientificName"
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-card-actions class="mx-7 mb-6">
+          <p class="font-weight-light ma-0">
+            Choose icon &emsp;
+          </p>
+          <v-btn-toggle v-model="icon">
+            <v-btn color="pink" icon outlined class="round mx-1">
+              <v-icon :color="icon === 0 ? 'pink' : 'gray'">
+                mdi-flower
+              </v-icon>
+            </v-btn>
+            <v-btn color="pink lighten-3" icon outlined class="round mx-1">
+              <v-icon :color="icon === 1 ? 'pink lighten-3' : 'gray'">
+                mdi-mushroom
+              </v-icon>
+            </v-btn>
+            <v-btn color="orange" icon outlined class="round mx-1">
+              <v-icon :color="icon === 2 ? 'orange' : 'gray'">
+                mdi-carrot
+              </v-icon>
+            </v-btn>
+            <v-btn color="light-green" icon outlined class="round mx-1">
+              <v-icon :color="icon === 3 ? 'light-green' : 'gray'">
+                mdi-leaf
+              </v-icon>
+            </v-btn>
+            <v-btn color="green" icon outlined class="round mx-1">
+              <v-icon :color="icon === 4 ? 'green' : 'gray'">
+                mdi-tree
+              </v-icon>
+            </v-btn>
+          </v-btn-toggle>
         </v-card-actions>
-      </v-window-item>
-    </v-window>
-  </v-card>
+      </v-row>
+      <v-row>
+        <v-textarea
+          clearable
+          clear-icon="cancel"
+          label="Description"
+          outlined
+          :value="plant.description"
+          class="mx-8"
+          rows="10"
+        ></v-textarea>
+      </v-row>
+      <v-card-actions class="px-4 pb-4">
+        <v-btn color="indigo" text @click="displayForm--">
+          BACK
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="indigo" dark>
+          SAVE
+        </v-btn>
+      </v-card-actions>
+    </v-window-item>
+  </v-window>
 </template>
 
 <script>
