dcd69d3f7bb6f06dbf377630cd112231ab1acc74
Drewbi <22423862@student.uwa.edu.au>
2020-03-07 13:29:55 +0800

    improvement(plant map): changed plant map to receive plant props

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-card height="100%" width="100%" tile>
-      <uwa-plant-map ref="plantMap" />
+      <uwa-plant-map :plants="plants" />
     </v-card>
   </v-layout>
 </template>
@@ -12,6 +12,18 @@ import UWAPlantMap from '~/components/UWAPlantMap.vue'
 export default {
   components: {
     'uwa-plant-map': UWAPlantMap
+  },
+  data: () => ({
+    plants: []
+  }),
+  mounted(){
+    this.loadPlants()
+  },
+  methods: {
+    async loadPlants() {
+      const data = await this.$axios.$get('/api/flora')
+      this.plants = data
+    },
   }
 }
 </script>
