80a494626a4a5afbeae69dcbc64cc20b93d78d45
Drewbi <22423862@student.uwa.edu.au>
2020-03-30 04:33:56 +0800

    Added zdog basic stuff plus mouse follow

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,33 +1,13 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row align="center" justify="center">
-      <div class="text-center">
-        <h1 v-if="error.statusCode === 404" class="display-2">
-          {{ pageNotFound }}
-        </h1>
-        <h1 v-else class="display-2 font-weight-bold">
-          {{ otherError }}
-        </h1>
-        <v-btn
-          color="primary"
-          to="/"
-          rounded
-          dark
-          nuxt
-          class="mt-4"
-        >
-          <v-icon dark class="mr-2">
-            arrow_back
-          </v-icon>
-          Return to Home page
-        </v-btn>
-      </div>
-    </v-row>
-  </v-container>
+  <div>
+    <canvas id="canvas" width="500" height="500" @onResize="handleResize" @mousemove="setMouse">
+    </canvas>
+  </div>
 </template>
 
-<!-- Don't change anything within script-->
 <script>
+/* eslint-disable vue/no-parsing-error */
+import zdog from "zdog"
 
 export default {
   layout: 'empty',
@@ -37,21 +17,69 @@ export default {
       default: null
     }
   },
-  head() {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  },
   data: () => ({
-    pageNotFound: '404 Not Found',
-    otherError: 'An error occurred'
-  })
+    illo: null,
+    illustrationZoom: 360,
+    canvas: null,
+    canvasWidth: 0,
+    canvasHeight: 0,
+    mouseFromCenterX: 0,
+    mouseFromCenterY: 0
+  }),
+  mounted() {
+    this.canvas = document.getElementById("canvas")
+    const rect = this.canvas.getBoundingClientRect()
+    this.canvasWidth = rect.width
+    this.canvasHeight = rect.height
+    this.illo = new zdog.Illustration({
+      element: this.canvas,
+      resize: true,
+      zoom: 1.5
+    })
+    this.addShapes()
+    this.faceMouse()
+  },
+  // ready () {
+  //   window.addEventListener('resize', this.handleResize)
+  // },
+  // beforeDestroy () {
+  //   window.removeEventListener('resize', this.handleResize)
+  // },
+  methods: {
+    // handleResize(){
+    //   const rect = this.canvas.getBoundingClientRect()
+    //   this.canvasWidth = rect.width
+    //   this.canvasHeight = rect.height
+    // },
+/* eslint-disable no-new */
+    addShapes(){
+      new zdog.Ellipse({
+        addTo: this.illo,
+        diameter: 80,
+        stroke: 20,
+        color: '#636',
+      })
+    },
+    faceMouse(){
+      this.illo.rotate.x = (this.mouseFromCenterY < 0 ? -1 : 1) * ((1 / 50) * Math.sqrt(Math.abs(this.mouseFromCenterY)))
+      this.illo.rotate.y = (this.mouseFromCenterX < 0 ? -1 : 1) * ((1 / 50) * Math.sqrt(Math.abs(this.mouseFromCenterX)))
+      this.illo.updateRenderGraph()
+      requestAnimationFrame( this.faceMouse )
+    },
+    setMouse(event){
+      const rect = this.canvas.getBoundingClientRect()
+      const mouseX = event.clientX - rect.left
+      const mouseY = event.clientY - rect.top
+      this.mouseFromCenterX = mouseX - Math.round( this.canvasWidth/2 )
+      this.mouseFromCenterY = mouseY - Math.round( this.canvasHeight/2 )
+    }
+  }
 }
 </script>
 
-<script src="https://unpkg.com/zdog@1/dist/zdog.dist.min.js"></script>
-
-
-<style scoped></style>
+<style scoped>
+  #canvas {
+    width: 100vw;
+    height: calc(100vh - 52px)
+  }
+</style>
