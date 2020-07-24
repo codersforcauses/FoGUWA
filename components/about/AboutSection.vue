04c6f3d9172d1b98902c5f084c8ebec62a950852
Drewbi <drewalexander986@gmail.com>
2020-07-24 18:04:27 +0800

    WIP about sesh

---
+++ components/about/AboutSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-layout>
+    <v-flex xs12 sm6 md3 lg2 align-self-center>
+      <v-img :src="src" contain />
+      {{ src }}
+    </v-flex>
+    <v-flex xs12 sm6 md9 lg10>
+      <v-card tile flat>
+        <v-card-title class="display-1">
+          {{ heading }}
+        </v-card-title>
+        <v-card-text>
+          <slot />
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String, 
+      default: ''
+    },
+    heading: {
+      type: String,
+      required: true
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,46 +1,49 @@
 <template>
   <v-container>
     <v-layout row wrap>
-      <v-flex xs12 sm6 md3 lg2 class="my-5">
-        <v-img :src="require('~/assets/images/FoG-logo.png')" contain />
-      </v-flex>
-      <v-flex xs12 sm6 md9 lg10>
-        <v-card-title class="display-1">
-          About Friends of the Grounds of UWA
-        </v-card-title>
-        <v-card-text style="text-align: justify">
-          <p>
-            Friends of the Grounds UWA (FoGUWA) was established in 2000 with
-            four core objectives:
-          </p>
-          <ol class="mb-3">
-            <li>
-              To encourage the University and wider Western Australian community
-              to take an active interest in UWA’s grounds as an important part
-              of the heritage of this State
-            </li>
-            <li>
-              To enhance the profile of the university’s grounds by developing
-              an annual program of activities
-            </li>
-            <li>
-              To provide information about the grounds through publications,
-              lectures and events
-            </li>
-            <li>
-              To raise funds to assist with special projects relating to the
-              grounds
-            </li>
-          </ol>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/friendsofthegrounds/"
-            class="primary--text"
-          >
-            FoGUWA Facebook page
-          </a>
-        </v-card-text>
-      </v-flex>
+      <about-section
+        :src="require('~/assets/images/cfc-logo.svg')"
+        heading="About this Map"
+      >
+        <v-img
+          :src="require('~/assets/images/cfc-logo.svg')"
+          :aspect-ratio="1 / 1"
+          contain
+          class="mx-3"
+        />
+      </about-section>
+      <about-section :src="require('~/assets/images/FoG-logo.png')" heading="About Friends of Grounds UWA">
+        <p>
+          Friends of the Grounds UWA (FoGUWA) was established in 2000 with
+          four core objectives:
+        </p>
+        <ol class="mb-3">
+          <li>
+            To encourage the University and wider Western Australian community
+            to take an active interest in UWA’s grounds as an important part
+            of the heritage of this State
+          </li>
+          <li>
+            To enhance the profile of the university’s grounds by developing
+            an annual program of activities
+          </li>
+          <li>
+            To provide information about the grounds through publications,
+            lectures and events
+          </li>
+          <li>
+            To raise funds to assist with special projects relating to the
+            grounds
+          </li>
+        </ol>
+        <a
+          target="_blank"
+          href="https://www.facebook.com/friendsofthegrounds/"
+          class="primary--text"
+        >
+          FoGUWA Facebook page
+        </a>
+      </about-section>
       <v-flex xs12 sm6 md3 lg2 class="py-3">
         <v-card tile flat class="white">
           <v-img
@@ -82,10 +85,23 @@
 </template>
 
 <script>
+import AboutSection from '~/components/about/AboutSection.vue'
 export default {
+  components: {
+    'about-section': AboutSection
+  },
   auth: false,
   head: () => ({
     title: 'About Us'
+  }),
+  data: () => ({
+    sections: [
+      {
+        image: '',
+        heading: 'About Friends of the Grounds of UWA',
+        text: ''
+      }
+    ]
   })
 }
 </script>
