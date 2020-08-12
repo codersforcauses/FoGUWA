4a403d261c87ed801242d944c20ce429881e52b6
Drewbi <drewalexander986@gmail.com>
2020-08-12 11:08:37 +0800

    White space footer change

--- components/other/Footer.vue
+++ components/other/Footer.vue
@@ -1,6 +1,8 @@
 <template>
   <v-footer class="px-4">
-    <div class="mx-1">Made with &#10084; by Coders for Causes</div>
+    <div class="mx-1">
+      Made with &#10084; by Coders for Causes
+    </div>
   </v-footer>
 </template>
 
