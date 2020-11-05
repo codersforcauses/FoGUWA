cc82afe9cea36d3495a33c7c6b17e3f4e00fea68
Drewbi <drewalexander986@gmail.com>
2020-11-06 00:34:40 +0800

    Add basic image edit

--- components/admin/PlantEdit.vue
+++ components/admin/PlantEdit.vue
@@ -33,9 +33,38 @@
       no-resize
     ></v-textarea>
     <v-card-actions class="mx-4 mb-6">
-      <p class="ma-0">
-        Choose icon &emsp;
-      </p>
+      <v-list>
+        <v-subheader>Images</v-subheader>
+        <v-list-item
+          v-for="(image, i) in plant.images"
+          :key="image"
+        >
+          <v-list-item-avatar>
+            <v-img
+              src="https://scontent.fper6-1.fna.fbcdn.net/v/t1.0-9/123584211_3660594127295060_801662606055952945_o.jpg?_nc_cat=108&ccb=2&_nc_sid=cdbe9c&_nc_ohc=h8xs05qslRAAX-S75VR&_nc_ht=scontent.fper6-1.fna&oh=9de3da8e62f9b2ccee3911fe0c1566b9&oe=5FCAA610"
+              max-height="100"
+              max-width="100"
+            >
+            </v-img>
+          </v-list-item-avatar>
+          <v-text-field
+            :value="plant.images[i]"
+            clearable
+            :label="`Image ${i + 1}`"
+            no-resize
+            outlined
+            dense
+            rows="1"
+            class="mx-4"
+            @blur="updateImage($event, i)"
+          >
+            {{ image }}
+          </v-text-field>
+        </v-list-item>
+      </v-list>
+    </v-card-actions>
+    <v-card-actions class="mx-4 mb-6">
+      <v-subheader>Icon</v-subheader>
       <v-btn-toggle :v-model="selectedBtn">
         <v-btn
           v-for="(icon, i) in getAllPlantIcons"
@@ -98,8 +127,18 @@ export default {
       return this.getIconIndex(this.plant.icon)
     }
   },
+  watch: {
+    plant: {
+      handler() {
+        
+      },
+      deep: true
+    }
+  },
   created() {
     this.plant = {...this.plantValue}
+    this.plant.images = [...this.plantValue.images]
+    this.imageIcons = [...this.plantValue.images]
   },
   methods: {
     getIconIndex(iconName){
@@ -111,6 +150,18 @@ export default {
         "lotus"
       ]
       return iconIndexList.indexOf(iconName);
+    },
+    updateImage(e, i){
+      this.plant.images[i] = e.target.value
+      console.log(this.plant.images)
+    },
+    getSrc(src){
+      try {
+        const realSrc = require(`~/assets/images/plants/${src}`)
+        return realSrc
+      } catch(err) {
+        return require(`~/assets/images/unknown.jpg`)
+      }
     },
     emitBack(){
       this.$emit('back')
