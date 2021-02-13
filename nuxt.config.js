20f12d245532e89930e214cfdc2a562ec5443160
Drewbi <drewalexander986@gmail.com>
2021-02-14 02:44:03 +0800

    add host

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,7 +33,8 @@ module.exports = {
     ]
   },
   server: {
-    port: process.env.PORT || 3000, // default: 3000
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0',
   },
   /*
    ** Customize the progress-bar color
