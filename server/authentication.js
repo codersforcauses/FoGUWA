cfab6d4ef747cced620fda1f2e2f17fbc7952bea
Drew Alexander <drewalexander986@gmail.com>
2021-02-14 19:47:45 +0800

    Update authentication.js

--- server/authentication.js
+++ server/authentication.js
@@ -17,7 +17,7 @@ const checkJwt = expressjwt({
     cache: true,
     rateLimit: true,
     jwksRequestsPerMinute: 5,
-    jwksUri: `https://fog-uwa.au.auth0.com/.well-known/jwks.json`
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
   }),
 
   // Validate the audience and the issuer.
